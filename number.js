9e6b106e4d7e732e0c8993903e838b04f02922fb
Ola Ipaye <ipayeola@outlook.com>
2023-07-22 13:57:10 +0100

    Added new js file and updated index.html file
    Linked number.js file to index.html file and then updated the javascript file with basic number data styles and let and const variables.

---
+++ number.js
@@ -0,0 +1,11 @@
+let radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi); // Logged the values of the let and const variables to the console.
+
+// Using number data stypes with Arithmetic operators
+// console.log(10 / 2);
+let result = radius % 3;
+
+console.log(result);
+
