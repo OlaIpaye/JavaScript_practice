de7ebc31004899e5f77bdc7eeab7ca0f8a9e4833
Ola Ipaye <ipayeola@outlook.com>
2023-07-22 15:41:18 +0100

    Created js folder and updated number.js file
    Created a folder to put all my js files into and then updated number.js file with shorthand notations.

---
+++ js files/number.js
@@ -0,0 +1,29 @@
+let radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi); // Logged the values of the let and const variables to the console.
+
+// Using number data types with Arithmetic operators (+, -, *, /, %, **) Use the order of operations PEDMAS/BIDMAS
+// console.log(10 / 2);
+// let result = radius % 3;
+
+// let result = pi * radius**2;
+
+/*
+let result = 5 * (10-3)**2; // Using PEDMAS, it outputs 245.
+
+console.log(result); */
+
+// Using Shorthand Notations ++, --, +=, -=, *=, /=
+let likes = 10;
+// likes = likes + 1;
+// likes++; // Used a shorthand notation ++ to add 1 to the value of the let variable, likes. It performs the same as the above likes variable reassignment and outputs 11.
+
+//likes--; // Used a shorthand notation -- to subtract 1 from the value of the let variable, likes.
+
+// likes += 10; // The += shorthand notation takes the value in the let variable, likes, and then add it to the 10. It outputs as 20.
+// likes -= 5; // The -= shorthand notation takes the value in the let variable, likes, and then subtract it from the 5. It outputs as 5.
+// likes *= 2; // The *= shorthand notation takes the value in the let variable, likes, and then multiplies it by 2. It outputs as 20.
+likes /= 2; // The /= shorthand notation takes the value in the let variable, likes, and then divides it by 2. It outputs as 5.
+console.log(likes);
+
