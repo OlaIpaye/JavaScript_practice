a41fa00fd54ad0891ecde9f74f303ac8a65e9155
Ola Ipaye <ipayeola@outlook.com>
2023-07-24 20:36:35 +0100

    Updated object.js file
    Added arrays, and how to store and get values in an array, and how to get the length of an array.

---
+++ js files/object.js
@@ -0,0 +1,20 @@
+// Arrays are used to store collections of things in such as strings and numbers.
+
+let greatNames = ["Ola", "Toby", "Bukola"];
+/*
+greatNames[1] = "Marley"; // Used the variable and [] to reassign the value with the index of 1, and override it to change it to Marley. 
+console.log(greatNames[1]); // Used the square bracket notation[] to get a single value from the list of values in the array in the variable. */
+
+/*
+let ages = ["17", "20", "23", "26", "29"];
+
+ages[3] = "36";
+console.log(ages[3]); */
+
+
+// let random = ["Ola", "Toby", "Florence", 30, 60];
+// console.log(random[2]);
+
+// console.log(greatNames.length); // The length property outputs the amount of values stored in the greatNames array, which is 3.
+
+
