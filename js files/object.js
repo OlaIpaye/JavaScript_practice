95feaa1a077cfbe23a6490b687f04b10c08130e1
Ola Ipaye <ipayeola@outlook.com>
2023-07-24 21:42:44 +0100

    Updated object.js file with more array function types
    Added pop, push, indexOf, join, and concat function types to the object.js file.

--- js files/object.js
+++ js files/object.js
@@ -18,3 +18,27 @@ console.log(ages[3]); */
 // console.log(greatNames.length); // The length property outputs the amount of values stored in the greatNames array, which is 3.
 
 
+
+// ARRAY FUNCTIONS - types of fucntions to use in an array.
+
+// let outcome = greatNames.join(", "); // Used the join function to join the values in the greatNames array together into one value seperated with a comma and space.
+
+// let outcome = greatNames.indexOf("Bukola"); // Used the indexOf function to get the index of the value of Bukola, inside the greatNames array, which is 2.
+
+// let outcome = greatNames.concat(["Ibrahim", "Testimony", "Micheal"]); // Used the concat function (concatenation function), to pass an argument to the greatNames array, and added the additional values in the concat function to the original values to create one whole array.
+
+let outcome = greatNames.push("Marley"); // Used the push function to add values to the greatNames array and output the length, which is 4 because the original array contains 3 values, then one more way added using the push function. The push function is called a destructive function bceause it changes the original value of the greatNames array.
+
+outcome = greatNames.pop(); // Used the pop function to take off the end value in the greatNames array. So I used .push("Marley") to add a value to the end of the greatNames array, and then I used the pop function to take if off. The pop function is also a destructive function because it changes the original value in the greatNames array.
+
+console.log(outcome);
+
+
+
+
+
+
+
+
+
+
