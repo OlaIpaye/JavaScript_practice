b3b4c53de1303928de78a8face35b81cb3294187
Ola Ipaye <ipayeola@outlook.com>
2023-08-28 16:00:34 +0100

    Updated dom.js file with the create and append methods.

--- js files/dom.js
+++ js files/dom.js
@@ -1,3 +1,23 @@
+// Create and Append properties
+
+// Create Element document.createElement() - used to dynamically create a new html element within the dom. It takes the html tag I want to create as an argument and returns a new element of the type.
+
+// Step 1: created a new div element dynamically and stored it in a const variable.
+const newDivElement = document.createElement("div");
+
+// Step 2: Customize the div element i dynamically created above
+newDivElement.textContent = "This div was created dynamically.";
+newDivElement.setAttribute("class", "dynamic-Div"); // this can be seen when i debug the div in browser
+newDivElement.setAttribute("style", "margin-bottom: 50px; color: red"); // Customised the div uing the setAttribute method to add color and margin-bottom
+
+// Step 3: Appended the div element I dynamically created to the dom
+const parentElement = document.getElementById("parent-Section");
+parentElement.appendChild(newDivElement);
+
+//
+
+//
+
 // textContent Property - used to access or modify the text content of an html element.
 
 // Used textContent to get the element's text content:
@@ -6,8 +26,7 @@ console.log(textDiv); // The text variable is now: 'This is some text!'
 
 // If you prefer to set the element's text content, you can do:
 document.getElementById("div1").textContent = "This text is different!";
-// The HTML for divA is now:
-// <div id="divA">This text is different!</div>
+// Dynamically updated the HTML for the DIV divA to: <div id="divA">This text is different!</div>
 
 // Used textContent to get the p element, and output the text inside the second paragraph in the html
 var allPara = document.getElementsByTagName("p");
